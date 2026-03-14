b933ee43b1eac0be448b15b61b077e778cc9fde9
Huy Bùi <huychanhkun1201@gmail.com>
2026-03-14 12:25:23 +0700

    add backend

---
+++ BackEnd/phone-store-backend/src/main/java/com/tmdt/phone_store_backend/PhoneStoreApplication.java
@@ -0,0 +1,13 @@
+package com.tmdt.phone_store_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneStoreApplication.class, args);
+	}
+
+}
