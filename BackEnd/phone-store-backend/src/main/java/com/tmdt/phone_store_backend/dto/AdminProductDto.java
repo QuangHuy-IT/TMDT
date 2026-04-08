8edf97f28c8aaea01baf69def898f37891f31d40
Huy Bùi <huychanhkun1201@gmail.com>
2026-04-08 09:28:41 +0700

    update crud product

---
+++ BackEnd/phone-store-backend/src/main/java/com/tmdt/phone_store_backend/dto/AdminProductDto.java
@@ -0,0 +1,24 @@
+package com.tmdt.phone_store_backend.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdminProductDto {
+
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private Integer stock;
+    private String description;
+    private List<String> images;
+    private Map<String, String> specifications;
+    private Boolean isFeatured;
+    private LocalDateTime createdAt;
+}
